b79fe502ac419f79bfd6f99093c0b829a53b976b
vovaa641 <68954624+vovaa641@users.noreply.github.com>
2023-06-01 21:58:41 +1000

    first commit

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     @vite('resources/css/app.css')
+    <link rel="stylesheet" href="./build/assets/app-20a13d61.css">
 </head>
 <body>
     <nav class="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,5 @@ use App\Http\Controllers\PostController;
 */
 
 Route::get('/', [\App\Http\Controllers\PostController::class, 'index'])->name('home');
-Route::get('{post:slug}', [PostController::class, 'show'])->name('view');
+Route::get('/{post:slug}', [PostController::class, 'show'])->name('view');
 Route::get('/category/{category:slug}', [PostController::class, 'byCategory'])->name('by-category');
